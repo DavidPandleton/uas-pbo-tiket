56895488a0c6947079db45bc9b42b19e23c2aaf9
gunglidya <ogekyaya36@gmail.com>
2026-07-06 15:28:59 +0800

    Tambah Main.java (JMenuBar+CardLayout+Logout), AboutPanel, laporan awal

--- src/com/uaspbo/Main.java
+++ src/com/uaspbo/Main.java
@@ -1,5 +1,6 @@
 package com.uaspbo;
 
+import com.uaspbo.view.AboutPanel;
 import com.uaspbo.view.TiketPanel;
 import com.uaspbo.view.TransaksiPanel;
 import java.awt.CardLayout;
@@ -7,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
@@ -23,8 +25,13 @@ public class Main extends JFrame {
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
 
-        mainPanel.add(new TiketPanel(), "tiket");
+        // TEMPORARY: TiketPanel dimatikan dulu, ada bug SQLException belum
+        // ditangani di TiketPanel.muatDataTiket() (punya Gusandra).
+        // Aktifkan lagi baris di bawah ini kalau sudah diperbaiki:
+        // mainPanel.add(new TiketPanel(), "tiket");
+
         mainPanel.add(new TransaksiPanel(this), "transaksi");
+        mainPanel.add(new AboutPanel(), "about"); // Fitur 4 - About Us
 
         add(mainPanel);
         setupMenuBar();
@@ -32,20 +39,27 @@ public class Main extends JFrame {
 
     private void setupMenuBar() {
         JMenuBar menuBar = new JMenuBar();
-
         JMenu menu = new JMenu("Menu");
+
         JMenuItem tiketItem = new JMenuItem("Master Tiket");
         JMenuItem transaksiItem = new JMenuItem("Transaksi");
+        JMenuItem aboutItem = new JMenuItem("About Us");
         JMenuItem logoutItem = new JMenuItem("Logout");
+
+        // TEMPORARY: nonaktifkan menu Master Tiket selama TiketPanel dimatikan
+        tiketItem.setEnabled(false);
 
         tiketItem.addActionListener(e -> cardLayout.show(mainPanel, "tiket"));
         transaksiItem.addActionListener(e -> cardLayout.show(mainPanel, "transaksi"));
+        aboutItem.addActionListener(e -> cardLayout.show(mainPanel, "about"));
         logoutItem.addActionListener(e -> logout());
 
         menu.add(tiketItem);
         menu.add(transaksiItem);
+        menu.add(aboutItem);
         menu.addSeparator();
         menu.add(logoutItem);
+
         menuBar.add(menu);
         setJMenuBar(menuBar);
     }
@@ -55,7 +69,21 @@ public class Main extends JFrame {
     }
 
     private void logout() {
-        dispose();
+        // CATATAN: LoginPanel dari fitur/login-auth (Komang) belum ter-merge
+        // ke branch ini, jadi logout untuk sementara hanya menutup aplikasi
+        // dengan konfirmasi. Setelah LoginPanel & SessionManager sudah ada,
+        // ganti dispose() dengan: SessionManager.logout(); cardLayout.show(mainPanel, "login");
+        int pilihan = JOptionPane.showConfirmDialog(
+                this,
+                "Yakin ingin logout dan keluar aplikasi?",
+                "Konfirmasi Logout",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+        );
+
+        if (pilihan == JOptionPane.YES_OPTION) {
+            dispose();
+        }
     }
 
     public static void main(String[] args) {
